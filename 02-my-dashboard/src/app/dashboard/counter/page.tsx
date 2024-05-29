74769d6e69f6241c826f9b98f2793387e4174ceb
Keneth Vasquez <kenethfvm2014@gmail.com>
2024-05-29 08:15:33 -0600

    add pokemons pages

---
+++ 02-my-dashboard/src/app/dashboard/counter/page.tsx
@@ -0,0 +1,16 @@
+import { CartCounter } from '@/shopping-cart/components';
+export const metadata = {
+ title: 'Shopping Cart',
+ description: 'Counter Page',
+};
+
+export default function CounterPage() {
+
+  return (
+    <div className="flex flex-col items-center justify-center w-full h-full">
+      <span>Productos de Carrito</span>
+      <CartCounter value={ 20 } />
+      
+    </div>
+  );
+}
---
+++ 02-my-dashboard/src/app/dashboard/pokemons/page.tsx
@@ -0,0 +1,30 @@
+import { PokemonGrid, PokemonsResponse, SimplePokemon } from "@/pokemons";
+
+
+export const metadata = {
+ title: '151 Pokémons',
+ description: '151 Pokémons',
+};
+
+const getPokemons = async( limit = 151, offset = 0): Promise<SimplePokemon[]> => {
+    const data: PokemonsResponse = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=${ limit }&offset=${ offset }`)
+    .then( res => res.json() )
+
+    const pokemons = data.results.map( pokemon => ({
+        id: pokemon.url.split('/').at(-2)!,
+        name: pokemon.name,
+    }) )
+
+    return pokemons;
+}
+
+export default async function PokemonsPage() {
+
+  const pokemons = await getPokemons()
+  return (
+    <div className="flex flex-col">
+      <span className="text-5xl mr-2">Listado de Pokémons <small>estático</small></span>
+      <PokemonGrid pokemons={ pokemons } />
+    </div>
+  );
+}
