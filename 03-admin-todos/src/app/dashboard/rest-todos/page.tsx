7b5988833b90caa6fd86f6a732e6dbe4fd5d1e98
Keneth Vasquez <kenethfvm2014@gmail.com>
2024-06-13 07:58:08 -0600

    add server actions

--- 03-admin-todos/src/app/dashboard/rest-todos/page.tsx
+++ 03-admin-todos/src/app/dashboard/rest-todos/page.tsx
@@ -1,15 +1,15 @@
-
-import prisma from "@/lib/prisma";
-import { NewTodo, TodosGrid } from "@/todos";
-import { useEffect } from "react";
+export const dynamic = 'force-dynamic';
+export const revalidate = 0;
+import prisma from "@/lib/prisma"
+import { NewTodo, TodosGrid } from "@/todos"
+import { useEffect } from "react"
 
 /* export const metadata = {
  title: 'Listado de Todos',
  description: 'Listado de Todos',
 }; */
 export default async function RestTodosPage() {
-
-  const todos = await prisma.todo.findMany({ orderBy: { description: 'asc' } })
+  const todos = await prisma.todo.findMany({ orderBy: { description: "asc" } })
 
   // useEffect(() => {
   //   fetch('/api/todos')
@@ -21,8 +21,8 @@ export default async function RestTodosPage() {
       <div className="w-full px-3 mx-5 mb-5">
         <NewTodo />
       </div>
-      
-      <TodosGrid todos={ todos }/>
+
+      <TodosGrid todos={todos} />
     </div>
-  );
+  )
 }
--- 03-admin-todos/src/todos/components/NewTodo.tsx
+++ 03-admin-todos/src/todos/components/NewTodo.tsx
@@ -1,54 +1,55 @@
-'use client';
+"use client"
 
-import { FormEvent, useState } from "react";
-import { IoTrashOutline } from "react-icons/io5";
-import * as todosApi from '@/todos/helpers/todos'
-import { useRouter } from "next/navigation";
+import { FormEvent, useState } from "react"
+import { IoTrashOutline } from "react-icons/io5"
+// import * as todosApi from "@/todos/helpers/todos"
+// import { useRouter } from "next/navigation"
+import { addTodo, deleteCompleted } from "../actions/todo-actions"
 
+export const NewTodo = () => {
+  // const router = useRouter()
 
-export const NewTodo = () => { 
+  const [description, setDescription] = useState("")
 
-    const router = useRouter()
+  const onSubmit = async (e: FormEvent) => {
+    e.preventDefault()
+    if (description.trim().length === 0) return
+    await addTodo(description)
+    setDescription("")
+  }
 
-    const [description, setDescription] = useState('')
-
-    const onSubmit = async( e: FormEvent ) => {
-        e.preventDefault();
-        if (description.trim().length === 0) return
-        const createdTodo = await todosApi.createTodo( description )
-        setDescription('')
-        console.log({createdTodo})
-        router.refresh()
-    }
-
-    const deleteCompleted = async() => {
-        await todosApi.deleteCompletedTodos()
-        router.refresh()
-    }
-
+  // const deleteCompleted = async () => {
+  //   // await todosApi.deleteCompletedTodos()
+  //   // router.refresh()
+  // }
 
   return (
-    <form onSubmit={ onSubmit } className='flex w-full'>
-      <input type="text"
-        onChange={ (e) => setDescription(e.target.value ) }
-        value={ description }
+    <form onSubmit={onSubmit} className="flex w-full">
+      <input
+        type="text"
+        onChange={(e) => setDescription(e.target.value)}
+        value={description}
         className="w-6/12 -ml-10 pl-3 pr-3 py-2 rounded-lg border-2 border-gray-200 outline-none focus:border-sky-500 transition-all"
-        placeholder="¿Qué necesita ser hecho?" />
+        placeholder="¿Qué necesita ser hecho?"
+      />
 
-      <button type='submit' className="flex items-center justify-center rounded ml-2 bg-sky-500 p-2 text-white hover:bg-sky-700 transition-all">
+      <button
+        type="submit"
+        className="flex items-center justify-center rounded ml-2 bg-sky-500 p-2 text-white hover:bg-sky-700 transition-all"
+      >
         Crear
       </button>
-      
-      <span className='flex flex-1'></span>
 
-      <button 
-        onClick={ () => deleteCompleted() }
-        type='button' className="flex items-center justify-center rounded ml-2 bg-red-400 p-2 text-white hover:bg-red-700 transition-all">
+      <span className="flex flex-1"></span>
+
+      <button
+        onClick={() => deleteCompleted()}
+        type="button"
+        className="flex items-center justify-center rounded ml-2 bg-red-400 p-2 text-white hover:bg-red-700 transition-all"
+      >
         <IoTrashOutline />
         <span className="ml-2">Borrar completados</span>
       </button>
-
-
     </form>
   )
 }
