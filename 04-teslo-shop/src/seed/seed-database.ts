98a84175cdc2d4754fe660a349167a3a2dd6b342
Keneth Vasquez <kfvasque@pichincha.com>
2024-07-24 16:27:53 -0600

    Add DB Seed for products, images and categories

---
+++ 04-teslo-shop/src/seed/seed-database.ts
@@ -0,0 +1,53 @@
+import { initialData } from "./seed";
+import prisma from "../lib/prisma";
+
+interface Abc {
+  aasd: String;
+}
+
+async function main() {
+  //Borrar registros de la base de datos
+  await prisma.productImage.deleteMany();
+  await prisma.product.deleteMany();
+  await prisma.category.deleteMany();
+
+  const { categories, products } = initialData;
+
+  //Categorias
+  const categoriesData = categories.map((name) => ({ name }));
+  await prisma.category.createMany({ data: categoriesData });
+
+  //Productos
+
+  const categoriesDB = await prisma.category.findMany();
+
+  const categoriesMap = categoriesDB.reduce((map, category) => {
+    map[category.name.toLowerCase()] = category.id;
+
+    return map;
+  }, {} as Record<string, string>); // ,string=shirt, strgin=category>
+
+  products.forEach(async (product) => {
+    const { type, images, ...rest } = product;
+    const dbProduct = await prisma.product.create({
+      data: {
+        ...rest,
+        categoryId: categoriesMap[type.toLowerCase()],
+      },
+    });
+
+    // Images
+    const imagesdata = images.map((image) => ({
+      url: image,
+      productId: dbProduct.id,
+    }));
+
+    await prisma.productImage.createMany({ data: imagesdata });
+  });
+  console.log("Seed ejecutado correctamente");
+}
+
+(() => {
+  if (process.env.NODE_ENV === "production") return;
+  main();
+})();
